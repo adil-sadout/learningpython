3be3ce3fb06545a175ba1a4885aaa07c3ca9dcaa
Adil Sadout <adelsadout@hotmail.com>
2020-03-06 14:55:25 +0100

    made class game_board

--- first time python/tictactoe.py
+++ first time python/tictactoe.py
@@ -1,7 +1,21 @@
 tictactoe = [[0,0,0],[0,0,0],[0,0,0]]
-print("   A  B  C")
-i = 1
-for count, row in enumerate(tictactoe):
-	print(count,row)
 
+
+class game_board(object):
+	tictactoe = [[0,0,0],[0,0,0],[0,0,0]]
+	def print_game():
+		print("   a  b  c")
+		for count, row in enumerate(tictactoe):
+			print(count,row)
+	def play_round(x,y):
+		print("   a  b  c")
+		tictactoe[x][y]=1
+		for count, row in enumerate(tictactoe):
+			print(count,row)
+
+game_board.print_game()
+
+game_board.play_round(1,1)
+
+game_board.print_game()
 
