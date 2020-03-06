ead8749b411e3f186f6e093a9292f1545cff2d24
Adil Sadout <adelsadout@hotmail.com>
2020-03-06 23:42:20 +0100

    added winner when horizontal line become equal

--- first time python/basicapp.py
+++ first time python/basicapp.py
@@ -2,3 +2,6 @@ prog_language = "python","java","c++"
 
 for languages in prog_language:
 	print(languages)
+
+
+
--- first time python/tictactoe.py
+++ first time python/tictactoe.py
@@ -1,24 +1,41 @@
-tictactoe = [[0,0,0],[0,0,0],[0,0,0]]
+tictactoe = [[1,2,3],[4,5,6],[7,8,9]]
+			
 
 
-class Game_Board(object):
-	tictactoe = [[0,0,0],[0,0,0],[0,0,0]]
-	def print_game():
-		print("   a  b  c")
+
+def print_game():
+	print("   0  1  2")
+	for count, row in enumerate(tictactoe):
+		print(count,row)
+
+
+
+def winner(lst,x):
+   return lst[x][1:] == lst[x][:-1]
+
+
+def play_round(x,y):
+	try:
+		print("   0  1  2")
+		tictactoe[x][y]=1
 		for count, row in enumerate(tictactoe):
+
 			print(count,row)
-	def play_round(x,y):
-		try:
-			print("   a  b  c")
-			tictactoe[x][y]=1
-			for count, row in enumerate(tictactoe):
-				print(count,row)
-		except:
-			print("something went wrong!!!")
 
-Game_Board.print_game()
+		if winner(tictactoe,x)== True:
+				print("You have won")
+		
+	except:
+		print("something went wrong!!!")
 
-Game_Board.play_round(1,1)
 
-Game_Board.print_game()
+
+
+play_round(0,1)
+play_round(0,2)
+
+
+
+
+
 
